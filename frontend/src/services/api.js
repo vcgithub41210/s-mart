4be821cc3a529d461750ac21ffa703f1605d4294
Manu <manu.saji@gmail.com>
2025-08-07 00:22:13 +0530

    fixed api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://s-mart-o9lm.onrender.com';
+const API_BASE_URL = 'https://s-mart-o9lm.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
