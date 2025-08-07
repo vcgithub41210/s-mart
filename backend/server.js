e390357348b4577cc24106668fd34256a105bc79
Manu2004 <117105122+Manu-2004@users.noreply.github.com>
2025-08-07 13:17:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['https://s-mart-1234.vercel.app'],
+  // origin: ['https://s-mart-1234.vercel.app'],
+  origin: ['https://s-mart-phi.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 }));
